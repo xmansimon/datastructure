039e6d43373f130cb9d5873ae85ad75c4a9a8011
Simon Su <xmansimon@live.com>
2018-09-25 23:09:53 -0400

    postorder

---
+++ treetraversal/postorder.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private void postorderTraversal(TreeNode root, List<Integer> answer) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal(root.left, answer);   // traverse left subtree
+        postorderTraversal(root.right, answer);
+        answer.add(root.val);                  // visit the root
+// traverse right subtree
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        postorderTraversal(root, answer);
+        return answer;
+    }
+}
