36c8d71a9d06ea82d435d000547d273640188794
Simon Su <xmansimon@live.com>
2018-09-27 02:35:58 -0400

    tree depth

--- treetraversal/treeDepth.java
+++ treetraversal/treeDepth.java
@@ -1,3 +1,7 @@
+/**
+ * top-down recursion
+ * }
+ */
 class treeDepth{
     private int answer;
     private void maximum_depth(TreeNode root, int depth) {
@@ -11,3 +15,20 @@ class treeDepth{
         maximum_depth(root.right, depth + 1);
     }
 }
+
+/**
+ * bottom-up recursion
+ * }
+ */
+
+/**
+ * 
+ * public int maximum_depth(TreeNode root) {
+	if (root == null) {
+		return 0;                                   // return 0 for null node
+	}
+	int left_depth = maximum_depth(root.left);
+	int right_depth = maximum_depth(root.right);
+	return Math.max(left_depth, right_depth) + 1;	// return depth of the subtree rooted at root
+    }
+ */
