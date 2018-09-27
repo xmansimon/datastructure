197a50de3b238a9e25be0dd15ecef3c0b1c9b0a7
Simon Su <xmansimon@live.com>
2018-09-27 02:16:30 -0400

    Tree Depth

---
+++ treetraversal/treeDepth.java
@@ -0,0 +1,13 @@
+class treeDepth{
+    private int answer;
+    private void maximum_depth(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            answer = Math.max(answer, depth);
+        }
+        maximum_depth(root.left, depth + 1);
+        maximum_depth(root.right, depth + 1);
+    }
+}
